d9564bf3dd118e58aa1ff61e44d599692049846c
akrotenko <id.v1cher@gmail.com>
2021-06-17 14:30:03 +0500

    add security password

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router()
 
@@ -20,7 +21,7 @@ router.post('/login', async (req, res) => {
         const {email, password} = req.body
         const candidate = await User.findOne({email})
         if (candidate) {
-            const areSave = password === candidate.password
+            const areSave = await bcrypt.compare(password, candidate.password)
             if (areSave) {
                 req.session.user = candidate
                 req.session.isAuthenticated = true
@@ -50,10 +51,11 @@ router.post('/register', async (req, res) => {
         if (candidate) {
             res.redirect('/auth/login#register')
         } else {
+            const hashPassword = await bcrypt.hash(password, 10)
             const user = new User({
                 email,
                 name,
-                password,
+                password: hashPassword,
                 card: {
                     items: []
                 }
